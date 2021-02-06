f3fe2c3f4a8e9023e943e611b52a587c8095d964
CihanKocak <69902606+CihanKocak@users.noreply.github.com>
2021-02-07 00:00:03 +0300

    06 Subat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductTest();
+            //CategoryTest();
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
-            {
-                Console.WriteLine(product.ProductName);
-            }
 
             Console.ReadLine();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
         }
     }
 }
